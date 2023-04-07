f826d4e7149737c14f3a1a26f434a450a510e7b5
MusaevaAdelya <msvadelya@gmail.com>
2023-04-07 12:22:08 +0600

    + added GET endpoint "/get/all-bets" in AdminController

--- app/src/main/java/com/example/app/controller/AdminController.java
+++ app/src/main/java/com/example/app/controller/AdminController.java
@@ -1,15 +1,13 @@
 package com.example.app.controller;
 
 import com.example.app.dto.RegisterHorse;
+import com.example.app.service.BetService;
 import com.example.app.service.HorseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,11 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AdminController {
     private final HorseService horseService;
+    private final BetService betService;
 
     @PostMapping("/add/horse")
     public ResponseEntity<Void> addNewHorse(@Valid @RequestBody RegisterHorse horse) {
         horseService.registerNewHorse(horse);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/get/all-bets")
+    public ResponseEntity<List<BetDto>> getAllActiveBets(){
+        ResponseEntity.ok().body(betService.getAllBets());
     }
 
 }
