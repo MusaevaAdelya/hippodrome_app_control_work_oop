b89c9014df1741a99f1b32ef9ca393b40636b041
MusaevaAdelya <msvadelya@gmail.com>
2023-04-07 12:33:57 +0600

    + added UserDTO and BetDTO

---
+++ app/src/main/java/com/example/app/dto/BetDTO.java
@@ -0,0 +1,14 @@
+package com.example.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BetDTO {
+    private HorseDTO horse;
+    private UserDTO user;
+    private Integer money;
+}
